d0e89f7ec13f2dd7eef78edeab1173fccde270ca
Tiffany Brand <brand2learn@gmail.com>
2020-11-07 21:45:00 -0500

    Fix pathname

--- src/pathnames.js
+++ src/pathnames.js
@@ -1,4 +1,4 @@
 const base = process.env.NODE_ENV === "production" ? "/react-ts-portfolio" : "";
-export const HOME_PATH = base || "/";
+export const HOME_PATH = base + "/";
 export const PORTFOLIO_PATH = base + "/portfolio";
 export const SKILLS_PATH = base + "/skills";
